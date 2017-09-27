842b78bd2255d7c04df9531a83432e9a7556fded
iZaq <p.paravar@gmail.com>
2017-09-27 17:11:51 +0330

    leap

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,7 @@
-def is_leap_year():
-    pass
+def is_leap_year(year):
+    if (year % 4 == 0) and ((year % 100 != 0) or (year % 400 == 0)):
+        print("The year %s is a leap year!") % (str(year))
+        return True
+    else:
+        print("The year %s is NOT a leap year.") % (str(year))
+        return False
