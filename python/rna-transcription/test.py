1fad3f1f678254605e293301e280fedb2a2cafe1
iZaq <p.paravar@gmail.com>
2017-09-30 13:47:10 +0330

    test

--- python/rna-transcription/test.py
+++ python/rna-transcription/test.py
@@ -1,6 +1,10 @@
-dna_to_rna = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
-dna = 'GCT'
-for s in range(3):
-    dna = dna.replace(dna[s], dna_to_rna[dna[s]])
-
-print(dna)
+def to_rna(dna):
+    dna_to_rna = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
+    dna = list(dna)
+    for s in range(len(dna)):
+        try:
+            dna[s] = dna_to_rna[dna[s]]
+        except:
+            dna = ''
+            break
+    return(''.join(dna))
